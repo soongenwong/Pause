bdae26b1443eb610719d85187827734d3ab011e9
Soong En Wong <wsoongen2015@gmail.com>
2025-07-04 23:24:58 +0100

    Initial Commit

---
+++ Pause/PauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseApp.swift
+//  Pause
+//
+//  Created by Soong En Wong on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PauseTests/PauseTests.swift
@@ -0,0 +1,17 @@
+//
+//  PauseTests.swift
+//  PauseTests
+//
+//  Created by Soong En Wong on 04/07/2025.
+//
+
+import Testing
+@testable import Pause
+
+struct PauseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
